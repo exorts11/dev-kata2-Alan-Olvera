dce716a2bfb2e9d3241937b1acc364111a0b0409
exorts11 <exorts@hotmail.com>
2023-08-19 23:13:12 -0600

    feat: ejercicio3

---
+++ js/3.5-while.js
@@ -0,0 +1,23 @@
+const days = ["lunes", "martes", "miercoles", "jueves", "sabado"];
+const response = ["Nacido", "Bautizado", "Casado", "Enfermo", "Empeoro", "Murio"];
+let entry = ""
+domingo = false;
+let validEntry = false
+
+while (!domingo){
+
+    entry = prompt("Introduce un dia de la semana")
+        
+    console.log("Solomon Grundy,")
+    for (k in days){
+        if(entry == days[k]){
+            console.log(response[k]+" en " + days[k] + ",")
+        }
+    }
+
+    if (entry == "domingo"){
+        console.log("Enterrado en domingo.\nEste es el fin de Solomon Grundy");
+        domingo = true
+    }
+
+}
